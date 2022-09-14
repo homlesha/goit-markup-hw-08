65c9824bf6b346e4a5ab92a5307ef4511aaf8ee3
Alexey Khomyak <homlesha18@gmail.com>
2022-09-14 15:11:40 +0300

    homework-8

---
+++ js/mob-menu.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-btn').addEventListener('click',()=> {
+document.querySelector('.mobile-menu').classList.toggle('active');
+document.querySelector('.menu-btn').classList.toggle('active');
+document.body.classList.toggle('overflow');
+});
